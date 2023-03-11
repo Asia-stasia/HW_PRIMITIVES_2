2e16c8e9a7db86f071c7c11957490b51ae81fa0e
Asia-stasia <124874660+Asia-stasia@users.noreply.github.com>
2023-03-12 00:10:15 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        int initialAmount = 100; 
-        int refillAmount = 1300; 
+        int initialAmount = 100;
+        int refillAmount = 300;
         int bonus = 0;
-        if (refillAmount > 1000); {
-        bonus = refillAmount / 100;
+        if (refillAmount > 1000) {
+            bonus = refillAmount / 100;
         }
-        int finalAmount = initialAmount + refillAmount + bonus; 
+        int finalAmount = initialAmount + refillAmount + bonus;
         System.out.println("Бонус - " + bonus + " рублей");
         System.out.println("Итоговый счет - " + finalAmount + " рублей");
     }
