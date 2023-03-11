6ba04b475db075fe1cbd5cda3fb5196138b91965
ANASTASIA <anastasia83-83@mail.ru>
2023-03-11 15:04:57 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 100; // a - сумма начального счета
+        int b = 1300; // b - сумма пополнения
+        int bonus;
+        if (b > 1000);
+        bonus = b / 100;
+        int c = a + b + bonus; // c - итоговый счет
+        System.out.println("Бонус - " + bonus + " рублей");
+        System.out.println("Итоговый счет - " + c + " рублей");
+    }
+}
